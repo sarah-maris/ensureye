53fc9969baecccb78c159e0b3b967a3ee29e301f
Sarah Maris <sarah@sarahmaris.com>
2016-11-19 18:48:49 -0500

    Add copyright to footer

---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ *
+ * Revised by Sarah Maris for ensureye.com
+ * Updated: 11-19-16
+ */
+
+?>
+<div class="site-info">
+	&copy; <?php $the_year = date("Y"); echo $the_year; ?> Intelligent Rx Solutions Inc.
+</div><!-- .site-info -->
